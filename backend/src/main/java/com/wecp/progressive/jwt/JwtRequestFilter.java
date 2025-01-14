ddf5d029b8676752f5da4eb71a0d3f6cb9650163
username <wecp-dummy@gmail.com>
2025-01-14 10:21:08 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/jwt/JwtRequestFilter.java
+++ backend/src/main/java/com/wecp/progressive/jwt/JwtRequestFilter.java
@@ -9,11 +9,16 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.SignatureException;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
@@ -40,6 +45,12 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
+
+            try{
+                username = jwtUtil.extractUsername(jwt);
+            }catch(ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | IllegalArgumentException e){     //SignatureException add if needed
+
+            }
             username = jwtUtil.extractUsername(jwt);
         }
 
