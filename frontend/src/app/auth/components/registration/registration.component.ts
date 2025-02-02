66b9874f7186ed8c20fcb2cf905850cfa5126a1e
Aditya Anand <105532223+AdityaAnand73@users.noreply.github.com>
2025-02-02 19:08:18 +0530

    Day 21 IPL

--- frontend/src/app/auth/components/registration/registration.component.ts
+++ frontend/src/app/auth/components/registration/registration.component.ts
@@ -1,48 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from 'src/app/ipl/types/User';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent implements OnInit {
-  registrationForm!: FormGroup;
-  successMessage: string | null = null;
-  errorMessage: string | null = null;
+export class RegistrationComponent {
+  registrationForm: FormGroup;
+  successMessage: string = '';
+  errorMessage: string = '';
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private authService: AuthService
-  ) {}
-
-  ngOnInit(): void {
-    this.registrationForm = this.formBuilder.group({
-      fullName: ['', Validators.required],
-      username: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(8)]],
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
+      username: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9]+$/)]],
       email: ['', [Validators.required, Validators.email]],
-      role: ["", [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[A-Z])(?=.*\d).+$/)]]
     });
   }
 
-  onSubmit(): void {
+  onSubmit() {
     if (this.registrationForm.valid) {
-      this.authService.createUser(this.registrationForm.value).subscribe(
-        response => {
-          this.successMessage = 'Registration successful!';
-          this.errorMessage = null;
-          this.registrationForm.reset();
-        },
-        error => {
-          this.errorMessage = error;
-          this.successMessage = null;
-        }
-      );
+      this.successMessage = 'Registration successful!';
+      this.errorMessage = '';
+      console.log('User Data:', this.registrationForm.value);
+      this.resetForm();
     } else {
-      this.errorMessage = 'Please fill out the form correctly.';
+      this.successMessage = '';
+      this.errorMessage = 'Please fill out all required fields correctly.';
     }
+  }
+
+  resetForm() {
+    this.registrationForm.reset();
   }
 }
