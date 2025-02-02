05feaf3d3fcb5cf3e03756a2cdd138d11975ccc2
Aditya Anand <105532223+AdityaAnand73@users.noreply.github.com>
2025-02-02 17:50:52 +0530

    Day 22 IPL

--- frontend/src/app/ipl/ipl.module.ts
+++ frontend/src/app/ipl/ipl.module.ts
@@ -2,42 +2,33 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { IplRoutingModule } from "./ipl-routing.module";
-<<<<<<< HEAD
 import { FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { TeamCreateComponent } from "./components/teamcreate/teamcreate.component";
 import { CricketerCreateComponent } from "./components/cricketercreate/cricketercreate.component";
 import { MatchCreateComponent } from "./components/matchcreate/matchcreate.component";
 import { Match } from "./types/Match";
+import { RouterModule } from "@angular/router";
+import { TicketBookingComponent } from "./components/ticketbooking/ticketbooking.component";
+import { VoteComponent } from "./components/vote/vote.component";
 
 @NgModule({
   declarations: [
     TeamCreateComponent,
     CricketerCreateComponent,
     MatchCreateComponent,
+    TicketBookingComponent,
+    VoteComponent,
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    HttpClientModule
-=======
-import { ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-
-@NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
     HttpClientModule,
->>>>>>> 223324dc07b45aab6ad765574a36f47daf7e3f8d
+    IplRoutingModule,
+    RouterModule
   ],
   exports: [
     
   ]
 })
-<<<<<<< HEAD
 export class IplModule {}
-=======
-export class IplModule {}
->>>>>>> 223324dc07b45aab6ad765574a36f47daf7e3f8d
