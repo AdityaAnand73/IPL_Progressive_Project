1af572d6a036ed1b5b5be731e6f9ef4c3e203241
Aditya Anand <105532223+AdityaAnand73@users.noreply.github.com>
2025-02-06 10:15:03 +0530

    Day 25

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,33 +1,25 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-    role: string | null;
 
-    constructor(private router: Router) { }
+  constructor(private router: Router) { }
 
-    ngOnInit(): void {
-        console.log(localStorage.getItem("role"));
-        this.role = localStorage.getItem("role");
-    }
+  role:string|null;
+  ngOnInit(): void {
 
-    logout(): void {
-        localStorage.removeItem('token')
-        this.router.navigate(["/auth"]);
-    }
+    console.log(localStorage.getItem("role"));
+    this.role=localStorage.getItem("role");
+  }
 
-}
-=======
-
-export class NavBarComponent{
-
-  
+  logout(): void {
+    localStorage.removeItem('token')
+    this.router.navigate(["/auth"]);
+  }
 
 }
->>>>>>> f65cd762c3d8b4c3e171215dd01fc22913fc6d3a
